6d538930dbadcdb10ac8c5b8f8d1f0a342dc4960
Seoyeon Kim <catneru@naver.com>
2019-05-27 10:07:37 +0900

    초기 커밋.

---
+++ TeaBagMaker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace TeaBagMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbList = new System.Windows.Forms.ComboBox();
+            this.btn_input = new System.Windows.Forms.Button();
+            this.lbl_Time = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCountDown = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // cbList
+            // 
+            this.cbList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbList.FormattingEnabled = true;
+            this.cbList.Location = new System.Drawing.Point(13, 13);
+            this.cbList.Name = "cbList";
+            this.cbList.Size = new System.Drawing.Size(240, 20);
+            this.cbList.TabIndex = 0;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.CbList_SelectedIndexChanged);
+            // 
+            // btn_input
+            // 
+            this.btn_input.Location = new System.Drawing.Point(13, 40);
+            this.btn_input.Name = "btn_input";
+            this.btn_input.Size = new System.Drawing.Size(240, 64);
+            this.btn_input.TabIndex = 1;
+            this.btn_input.Text = "담그기!";
+            this.btn_input.UseVisualStyleBackColor = true;
+            this.btn_input.Click += new System.EventHandler(this.Btn_input_Click);
+            // 
+            // lbl_Time
+            // 
+            this.lbl_Time.AutoSize = true;
+            this.lbl_Time.Location = new System.Drawing.Point(13, 111);
+            this.lbl_Time.Name = "lbl_Time";
+            this.lbl_Time.Size = new System.Drawing.Size(41, 12);
+            this.lbl_Time.TabIndex = 2;
+            this.lbl_Time.Text = "시간 : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2019년 05월 27일 | 3503 김서연";
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.Font = new System.Drawing.Font("굴림", 25F);
+            this.txtCountDown.Location = new System.Drawing.Point(15, 127);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(238, 46);
+            this.txtCountDown.TabIndex = 4;
+            this.txtCountDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 213);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_Time);
+            this.Controls.Add(this.btn_input);
+            this.Controls.Add(this.cbList);
+            this.Name = "Form1";
+            this.Text = "Tea Making";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbList;
+        private System.Windows.Forms.Button btn_input;
+        private System.Windows.Forms.Label lbl_Time;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCountDown;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ TeaBagMaker/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic;
+
+namespace TeaBagMaker
+{
+    public partial class Form1 : Form
+    {
+        int CountOrgNum = 0;
+        string[] SList = new string[]{"홍차", "녹차", "루이보스차",
+            "국화차"};
+        int[] TList = new int[] { 2,3,5,2 };
+
+        string orgStr = ""; // 선택 결과 저장
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < SList.Length; i++)
+            {
+                this.cbList.Items.Add(this.SList[i]);
+            }
+            this.orgStr = this.lbl_Time.Text;
+            if (this.SList.Length > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+        }
+
+        private void CbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lbl_Time.Text = this.orgStr + TList[cbList.Items.IndexOf(cbList.Text)] + "분";
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (CountOrgNum < 1)
+            {
+                MessageBox.Show("티백을 건지세요!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                CountOrgNum = 1;
+                this.cbList.Focus();
+            }
+            else
+            {
+                this.CountOrgNum--;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum/60) + "분" + Convert.ToString(this.CountOrgNum%60) + "초";
+            }
+        }
+
+        private void Btn_input_Click(object sender, EventArgs e)
+        {
+            processTimer();
+        }
+        private void processTimer()
+        {
+            this.CountOrgNum = TList[cbList.Items.IndexOf(cbList.Text)]*60;
+            this.timer.Enabled = true;
+        }
+    }
+}
+
+
